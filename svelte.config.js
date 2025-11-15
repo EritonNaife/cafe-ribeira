a964e349849a2bb5869272b5a02422a250abf34d
Eriton <up202008859@up.pt>
2025-11-15 22:03:48 +0000

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	preprocess: [vitePreprocess({})],
+	kit: {
+		adapter: adapter()
+	}
+};
 
 export default config;
